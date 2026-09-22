59d24bdb3f9c14e42c9e1f8d58b84d8467f9653d
vlntddn <valentinddn2803@gmail.com>
2026-09-22 08:58:11 +0200

    Remove basePath from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-  basePath: "/branding-agency-site",
+    output: "export",
+    trailingSlash: true,
 };
 
 export default nextConfig;
